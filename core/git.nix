07979a46d1f208866c8d05ebe1f1f76f8dd5d4ef
Mujahid Siyam <mujaxso@proton.me>
2025-10-05 00:43:33 +0200

    add: git

--- core/git.nix
+++ core/git.nix
@@ -1,20 +1,45 @@
-{
-  email,
-  username,
-  ...
-}: {
+{ config, pkgs,lib, ... }: {
+  
   programs.git = {
     enable = true;
-    userName = "${username}";
-    userEmail = "${email}";
-    signing = {
-      key = "~/.ssh/id_ed25519.pub";
-      signByDefault = true;
-    };
-    extraConfig = {
-      init.defaultBranch = "main";
-      commit.gpgSign = true;
-      gpg.format = "ssh";
-    };
+    # userName  = "Mujahid Siyam";
+    # userEmail = "mujaxso@proton.me";
+    # signing = {
+    #   key = "~/.ssh/id_ed25519.pub";
+    #   signByDefault = true;
+    # };
+    # aliases = {
+    #   co = "checkout";
+    #   br = "branch";
+    #   ci = "commit";
+    #   st = "status";
+    #   lg = "log --oneline --graph --decorate";
+    # };
+
+    # extraConfig = {
+    #   core.editor = "nvim";
+    #   init.defaultBranch = "main";
+    #   pull.rebase = false;
+    #   commit.gpgSign = true;
+    #   gpg.format = "ssh";
+    # };
+
+    # includes = [
+    #   {
+    #     condition = "gitdir:~/Dev/";
+    #     contents.user = {
+    #       email = "personal@email.com";
+    #       name = "My Name";
+    #     };
+    #   }
+    #   {
+    #     condition = "gitdir:~/Work/";
+    #     contents.user = {
+    #       email = "work@email.com";
+    #       name = "My Name";
+    #     };
+    #   }
+    # ];
   };
 }
+
