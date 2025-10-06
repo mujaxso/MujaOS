a3b04c241f97c9bbc16fb36dae08375cb9b77fdf
Mujahid Siyam <mujaxso@proton.me>
2025-10-06 22:57:50 +0200

    add: sddm theme chili

--- modules/desktop/sddm.nix
+++ modules/desktop/sddm.nix
@@ -1,11 +1,21 @@
 { config, pkgs, ... }:
+
 {
   services.xserver.enable = true;
 
   services.displayManager.sddm = {
     enable = true;
     wayland.enable = true;
+    theme = "chili";
   };
-    services.displayManager.sessionPackages = [ pkgs.hyprland ];
 
+  services.displayManager.sessionPackages = [
+    pkgs.hyprland
+  ];
+
+  environment.systemPackages = with pkgs; [
+    sddm-chili-theme
+    qt5.qtquickcontrols
+    qt5.qtgraphicaleffects
+  ];
 }
