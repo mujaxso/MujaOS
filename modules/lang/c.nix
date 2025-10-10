5c31511407359808955d2a837c329fb47fd1477b
Mujahid Siyam <mujaxso@proton.me>
2025-10-10 15:19:42 +0200

    add: zig language

--- modules/lang/c.nix
+++ modules/lang/c.nix
@@ -1,14 +1,14 @@
 { pkgs, ...}:
 {
   environment.systemPackages = with pkgs; [
-       libllvm
-       clang
-       clang-tools
-       cmake
-       gnumake
-       libtool
-       libvterm
-       glibtool
-       
-     ];
-   }
+    libllvm
+    clang
+    clang-tools
+    cmake
+    gnumake
+    libtool
+    libvterm
+    glibtool
+    
+  ];
+}
--- modules/lang/lang.nix
+++ modules/lang/lang.nix
@@ -3,11 +3,13 @@
 let
   # define each imported module
   c = ./c.nix;
+  zig = ./zig.nix;
   rust = ./rust.nix;
   nodejs = ./nodejs.nix;
 in {
   imports = [
     c
+    zig
     rust
     nodejs
   ];
