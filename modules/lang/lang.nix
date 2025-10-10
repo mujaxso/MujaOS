5b803904fb75a434b9691bc0d940aa16bd9bd7ba
Mujahid Siyam <mujaxso@proton.me>
2025-10-10 15:25:00 +0200

    add: go language

--- modules/lang/lang.nix
+++ modules/lang/lang.nix
@@ -5,12 +5,14 @@ let
   c = ./c.nix;
   zig = ./zig.nix;
   rust = ./rust.nix;
+  go = ./go.nix;
   nodejs = ./nodejs.nix;
 in {
   imports = [
     c
     zig
     rust
+    go
     nodejs
   ];
 }
