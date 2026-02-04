466b6393ba6e8578098198d877995e7aa4db15ec
Mujahid Siyam <mujaxso@proton.me>
2026-02-04 16:12:41 +0100

    add: davinci resolve

--- modules/programs/videos/davinci-resolve.nix
+++ modules/programs/videos/davinci-resolve.nix
@@ -1,16 +1,23 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
-    davinci-resolve
+    (davinci-resolve.overrideAttrs (oldAttrs: {
+      postInstall =
+        (oldAttrs.postInstall or "")
+        + ''
+          sed -i 's/VideoEditing/X-VideoEditing/g' $out/share/applications/*.desktop || true
+        '';
+    }))
   ];
 
   xdg.desktopEntries.davinci-resolve = {
     name = "DaVinci Resolve";
     genericName = "Video Editor";
-    exec = "env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia davinci-resolve";
+    exec = "env __NV_PRIME_RENDER_OFFLOAD=1 __GLX_VENDOR_LIBRARY_NAME=nvidia davinci-resolve %U";
     icon = "davinci-resolve";
     type = "Application";
     terminal = false;
-    categories = ["AudioVideo" "VideoEditing"];
+    categories = ["AudioVideo" "Video"];
+    mimeType = ["video/x-matroska" "video/mp4" "video/quicktime" "video/x-msvideo"];
     comment = "Professional video editing software";
   };
 }
